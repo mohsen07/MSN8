17ecd66b723bac655bedc673afdcb55bda7f277e
mohammadhatamiorg <mohammadhatamiorg@gmail.com>
2024-06-24 09:31:34 -0700

    Revert "skeleton"
    This reverts commit 861e0379aa0e79e4854a4f0378e7c5a18e7bb8c5.

--- src/main/webapp/app/metronic/_metronic/layout/layout.module.ts
+++ src/main/webapp/app/metronic/_metronic/layout/layout.module.ts
@@ -1,43 +1,42 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {InlineSVGModule} from 'ng-inline-svg-2';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InlineSVGModule } from 'ng-inline-svg-2';
+import { RouterModule, Routes } from '@angular/router';
 import {
   NgbDropdownModule,
   NgbProgressbarModule,
   NgbTooltipModule,
 } from '@ng-bootstrap/ng-bootstrap';
-import {TranslateModule} from '@ngx-translate/core';
-import {TranslationModule} from '../../modules/i18n';
-import {LayoutComponent} from './layout.component';
-import {ExtrasModule} from '../partials/layout/extras/extras.module';
-import {Routing} from '../../pages/routing';
-import {HeaderComponent} from './components/header/header.component';
-import {ContentComponent} from './components/content/content.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {ScriptsInitComponent} from './components/scripts-init/scripts-init.component';
-import {ToolbarComponent} from './components/toolbar/toolbar.component';
-import {TopbarComponent} from './components/topbar/topbar.component';
-import {PageTitleComponent} from './components/header/page-title/page-title.component';
-import {HeaderMenuComponent} from './components/header/header-menu/header-menu.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationModule } from '../../modules/i18n';
+import { LayoutComponent } from './layout.component';
+import { ExtrasModule } from '../partials/layout/extras/extras.module';
+import { Routing } from '../../pages/routing';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ScriptsInitComponent } from './components/scripts-init/scripts-init.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { PageTitleComponent } from './components/header/page-title/page-title.component';
+import { HeaderMenuComponent } from './components/header/header-menu/header-menu.component';
 import {
   DrawersModule,
   DropdownMenusModule,
   ModalsModule,
-} from '../partials';
-import {ThemeModeModule} from '../partials/layout/theme-mode-switcher/theme-mode.module';
-import {SidebarComponent} from './components/sidebar/sidebar.component';
-import {SidebarLogoComponent} from './components/sidebar/sidebar-logo/sidebar-logo.component';
-import {SidebarMenuComponent} from './components/sidebar/sidebar-menu/sidebar-menu.component';
-import {SidebarFooterComponent} from './components/sidebar/sidebar-footer/sidebar-footer.component';
-import {NavbarComponent} from './components/header/navbar/navbar.component';
-import {AccountingComponent} from './components/toolbar/accounting/accounting.component';
-import {ClassicComponent} from './components/toolbar/classic/classic.component';
-import {ExtendedComponent} from './components/toolbar/extended/extended.component';
-import {ReportsComponent} from './components/toolbar/reports/reports.component';
-import {SaasComponent} from './components/toolbar/saas/saas.component';
+ } from '../partials';
+ import { ThemeModeModule } from '../partials/layout/theme-mode-switcher/theme-mode.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarLogoComponent } from './components/sidebar/sidebar-logo/sidebar-logo.component';
+import { SidebarMenuComponent } from './components/sidebar/sidebar-menu/sidebar-menu.component';
+import { SidebarFooterComponent } from './components/sidebar/sidebar-footer/sidebar-footer.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { AccountingComponent } from './components/toolbar/accounting/accounting.component';
+import { ClassicComponent } from './components/toolbar/classic/classic.component';
+import { ExtendedComponent } from './components/toolbar/extended/extended.component';
+import { ReportsComponent } from './components/toolbar/reports/reports.component';
+import { SaasComponent } from './components/toolbar/saas/saas.component';
 import {SharedModule} from "../shared/shared.module";
-import {BrowserModule} from "@angular/platform-browser";
 
 const routes: Routes = [
   {
@@ -58,7 +57,7 @@ const routes: Routes = [
     TopbarComponent,
     PageTitleComponent,
     HeaderMenuComponent,
-    SidebarComponent,
+     SidebarComponent,
     SidebarLogoComponent,
     SidebarMenuComponent,
     SidebarFooterComponent,
@@ -79,7 +78,7 @@ const routes: Routes = [
     ExtrasModule,
     ModalsModule,
     DrawersModule,
-    DropdownMenusModule,
+     DropdownMenusModule,
     NgbTooltipModule,
     TranslateModule,
     ThemeModeModule,
@@ -87,5 +86,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
 })
-export class LayoutModule {
-}
+export class LayoutModule {}
